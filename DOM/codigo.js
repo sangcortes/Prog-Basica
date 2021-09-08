e7be730d6bdaab133feb99547197fc5769ac2d6f
Santiago Gutierrez <sangc_93@hotmail.com>
2021-09-08 14:20:42 -0500

    Creacion repositorio programacion basica

---
+++ DOM/codigo.js
@@ -0,0 +1,5 @@
+document.write("Hola estudiante");
+            var ruta = window.location;
+            console.log(ruta);
+            console.log(console);
+            document.write("Estas en: "+ ruta)
