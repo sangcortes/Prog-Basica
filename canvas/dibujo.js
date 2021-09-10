962d99b70e71ff119983f4ed0244709c6e6264bb
Santiago Gutierrez <sangc_93@hotmail.com>
2021-09-09 19:13:12 -0500

    Agregar color de dibujo

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,31 +1,10 @@
 var texto = document.getElementById("texto_lineas");
 var boton = document.getElementById("boton1");
+var color_sel = document.getElementById("color_ppal");
 boton.addEventListener("click", dibujoPorClick);
 var d=document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var lineas1 = 30;
-var l = 0;
-var n = 0;
-var yi, xf;
-var color1 = "#FAA";
-console.log (window);
-for(l = 0; l < lineas; l++)
-{
-    yi = 10 * l;
-    xf = 10 * (l+1);
-    dibujarLinea(color1,0,yi,xf,300);
-}
-
-for(n = 0; n < lineas1; n++)
-{
-    yi = 10 * n;
-    xf = 10 * (n+1);
-    dibujarLinea(color1,300,yi,xf,0);
-}
-
-dibujarLinea(color1, 1, 1, 1, 299);
-dibujarLinea(color1, 1, 299, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -36,7 +15,31 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
     lienzo.stroke();
     lienzo.closePath();
 }
+
 function dibujoPorClick()
 {
-    alert("No me toques ahi");
+    var lineas = parseInt(texto.value);
+    var lineas1 = parseInt(texto.value);
+    var l = 0;
+    var n = 0;
+    var yi, xf;
+    var color1 = color_sel.value;
+    var espacio = ancho/lineas
+    console.log (window);
+    for(l = 0; l < lineas; l++)
+    {
+        yi = espacio * l;
+        xf = espacio * (l+1);
+        dibujarLinea(color1,0,yi,xf,300);
+    }
+
+    for(n = 0; n < lineas1; n++)
+    {
+        yi = espacio * n;
+        xf = espacio * (n+1);
+        dibujarLinea(color1,300,yi,xf,0);
+    }
+
+    dibujarLinea(color1, 1, 1, 1, ancho-1);
+    dibujarLinea(color1, 1, ancho-1, ancho-1, ancho-1);  
 }
